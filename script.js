0e8a6f1b5aa248f1382ddf68b7997d8af7af39ba
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-10 17:21:47 +0800

    Add playRound function to script.js

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ function getComputerChoice() {
       return "scissors"
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+    return "Invalid Input!"
+  }
+
+  if (playerSelection == computerSelection) {
+    return "Tie!";
+  }
+
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock";
+  }
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors";
+  }
+
+  if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper";
+  }
+
+  if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors";
+  }
+
+  if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats Paper";
+  }
+
+  if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock";
+  }
+}

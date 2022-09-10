a3c833528dbd11dcbb60d83d25211c43c67a477f
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-10 17:57:04 +0800

    Add game function to script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
   }
 
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You Lose! Paper beats Rock";
+    return "You Lose... Paper beats Rock";
   }
 
   if (playerSelection == "rock" && computerSelection == "scissors") {
@@ -35,14 +35,45 @@ function playRound(playerSelection, computerSelection) {
   }
 
   if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You Lose! Rock beats Scissors";
+    return "You Lose... Rock beats Scissors";
   }
 
   if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You Lose! Scissors beats Paper";
+    return "You Lose... Scissors beats Paper";
   }
 
   if (playerSelection == "paper" && computerSelection == "rock") {
     return "You Win! Paper beats Rock";
   }
+}
+
+function game() {
+  let yourScore = 0;
+  let compScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let yourSelection = prompt("Choose a handsign");
+    let compSelection = getComputerChoice();
+    let result = playRound(yourSelection, compSelection);
+
+    console.log(result);
+
+    if (result == "Invalid Input!") {
+      alert("You are disqualified!");
+      return;
+    }
+
+    if (result == "Tie!") {
+      yourScore += 0.5;
+      compScore += 0.5;
+    } else if (result[4] === "L") {
+      compScore++;
+    } else if (result[4] === "W") {
+      yourScore++;
+    }
+  }
+
+  if (yourScore > compScore) console.log("You win the game!");
+  if (compScore > yourScore) console.log("You lose the game...");
+  if (yourScore === compScore) console.log("It's a draw!");
 }

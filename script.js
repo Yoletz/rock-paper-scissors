7bdbd496ad489773393f2fb2645633da41e58be4
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-16 11:32:55 +0800

    Edit index.html, edit style.css, rewrite script.js

--- script.js
+++ script.js
@@ -1,82 +1,89 @@
+const buttons = document.querySelectorAll("#buttons > button");
+buttons.forEach(btn => btn.addEventListener("click", startMatch));
+
+const yourScoreBar = document.querySelector("#your-score");
+const compScoreBar = document.querySelector("#comp-score");
+
+const rematchBtn = document.createElement("button");
+rematchBtn.setAttribute("id", "rematch");
+rematchBtn.addEventListener("click", resetBattle);
+rematchBtn.textContent = "Rematch";
+
+const result = document.querySelector("#result");
+const body = document.querySelector("body");
+
+let yourPoints = 0;
+let compPoints = 0;
+
+function resetBattle() {
+  body.removeChild(rematchBtn);
+  yourScoreBar.textContent = "Your Score: 0";
+  compScoreBar.textContent = "Comp Score: 0";
+  result.textContent = "";
+  for (const btn of buttons) {
+    btn.removeAttribute("disabled", "");
+  }
+}
+
+function startMatch() {
+  const points = playRound(this.id, getComputerChoice());
+  yourPoints += points.player;
+  compPoints += points.comp;
+  yourScoreBar.textContent = `Your Score: ${yourPoints}`;
+  compScoreBar.textContent = `Comp Score: ${compPoints}`;
+
+  if (compPoints === 5 || yourPoints === 5) {
+    body.append(rematchBtn);
+
+    if (yourPoints === 5) {
+      result.textContent = "You won the game!!";
+    } else if (compPoints === 5) {
+      result.textContent = "You lost the game...";
+    }
+
+    yourPoints = 0;
+    compPoints = 0;
+
+    for (const btn of buttons) {
+      btn.setAttribute("disabled", "");
+    }
+  }
+}
+
 function getComputerChoice() {
   let choiceNum = Math.ceil(Math.random() * 3);
 
   switch (choiceNum) {
     case 1:
-      return "Rock";
+      return "rock";
     case 2:
-      return "Paper";
+      return "paper";
     case 3:
-      return "Scissors";
-  }
-}
-
-function capitalize(str) {
-  str = str.toLowerCase();
-  str = str[0].toUpperCase() + str.slice(1);
-  return str;
-}
-
-function startBattle(playerSelection, computerSelection) {
-
-  if ((playerSelection == "Rock" && computerSelection == "Paper") ||
-    (playerSelection == "Scissors" && computerSelection == "Rock") ||
-    (playerSelection == "Paper" && computerSelection == "Scissors")) {
-      return `You Lose... ${computerSelection} beats ${playerSelection}`;
-  }
-
-  if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper") || 
-    (playerSelection == "Paper" && computerSelection == "Rock")) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
+      return "scissors";
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = capitalize(playerSelection);
-
-  if (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-    return "Invalid Input!";
-  }
 
   if (playerSelection == computerSelection) {
-    return "Tie!";
+    console.log("Tie!");
+    result.textContent = "Tie!";
+    return {"player": 0, "comp": 0};
   }
 
-  return startBattle(playerSelection, computerSelection);
-}
-
-function logResult(yourScore, compScore) {
-  if (yourScore > compScore) console.log("You win the game!");
-  if (compScore > yourScore) console.log("You lose the game...");
-  if (yourScore === compScore) console.log("It's a draw!");
-}
-
-function game() {
-  let yourScore = 0;
-  let compScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let yourSelection = prompt("Choose a handsign");
-    let compSelection = getComputerChoice();
-    let result = playRound(yourSelection, compSelection);
-
-    console.log(result);
-
-    if (result == "Invalid Input!") {
-      alert("You are disqualified!");
-      return;
-    }
-
-    if (result == "Tie!") {
-      yourScore += 0.5;
-      compScore += 0.5;
-    } else if (result.slice(0, 11) == "You Lose...") {
-      compScore++;
-    } else if (result.slice(0, 8) == "You Win!") {
-      yourScore++;
-    }
+  if ((playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") || 
+    (playerSelection == "paper" && computerSelection == "rock")) {
+      console.log(`You Win!! ${playerSelection} beats ${computerSelection}`);
+      result.textContent = "You scored!";
+      return {"player": 1, "comp": 0};
   }
 
-  logResult(yourScore, compScore);
+  if ((playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "scissors" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissors")) {
+      console.log(`You Lose... ${computerSelection} beats ${playerSelection}`);
+      result.textContent = "Computer scored.."
+      return {"player": 0, "comp": 1};
+  }
 }
